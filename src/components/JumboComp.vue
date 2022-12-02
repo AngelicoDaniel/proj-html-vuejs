1f0ecd0a6828f3536d34741e58677d2f975cd997
Daniel <angelicodaniel93@gmail.com>
2022-12-02 21:49:55 +0100

    jumbo completo

--- src/components/JumboComp.vue
+++ src/components/JumboComp.vue
@@ -2,12 +2,45 @@
     <section>
         <img src="../assets/img/our-team.png" alt="bg-parralax" class="team">
 
+        <div class="container w-50">
+            <!-- logo -->
+            <div class="row d-flex justify-between align-items-center py-3 m-auto">
+                <div class="col-6">
+                    <img class="nex" src="../assets/img/logo.png" alt="nex-gen-logo">
+                </div>
+            <!--nav-->
+                <div class="col-6 d-flex justify-content-around">
+                    <ul class="d-flex">
+                        <li class="mx-2" v-for="(elem, index) in navItems" :key="index">{{elem}}</li>
+                    </ul>
+                </div>
+            </div>
+
+            <!--slogan-->
+            <div class="h-100 d-flex flex-column align-items-center text-center my-5">
+                <h1>Ready <span class="highlighted-green">Team</span></h1>
+                <p class="p-3">
+                    No matter what your company needs, we will be ready to assist you in the
+                    best possible way.
+                </p>
+                <div class="d-flex">
+                    <a href="#" class="btn btn-green">GET IN TOUCH</a>
+                    <a href="#" class="btn btn-bg-white">READ MORE</a>
+                </div>
+            </div>
+        </div>
+
     </section>
 </template>
 
 <script>
     export default {
-        name: 'JumboComp'
+        name: 'JumboComp',
+        data() {
+            return {
+                navItems: ['HOME', 'SERVICES', 'ABOUT', 'PROJECTS', 'RESULTS', 'GET IN TOUCH']
+            }
+        }
     }
 </script>
 
@@ -18,12 +51,45 @@ section{
     background-repeat: no-repeat;
     position: relative;
     height: 95vh;
+    white-space: nowrap;
     .team{
         position: absolute;
         top: -16%;
         left: 6%;
-      
     }
+    .nex{
+        height: 60px;
+    }
+    a{
+        text-decoration: none;
+        margin: 20px;
+    }
+    .btn{
+        padding: 0.5rem 1rem;
+        border-radius: 2px;
+        text-decoration: none;
+        display: inline-block;
+        flex-shrink: 0;
+        cursor: pointer;
+    }
+    .highlighted-green{
+        color: #058283;
+        background-color: #d5e7e8;
+        border-radius: 7px;
+        padding-inline: 0.5rem;
+        opacity: 0.8;
+    }
+    .btn-green{
+        background-color: #058283;
+        color: white;
+    }
+    .btn-bg-white{
+        color: #058283;
+        border: 1px solid #058283;
+        background-color: white;
+    }
+
+
 }
 
 
