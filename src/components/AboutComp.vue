d3516f2a7a2909b7d88316a2764761a5d629d456
Daniel <angelicodaniel93@gmail.com>
2022-12-03 11:50:42 +0100

    about img

--- src/components/AboutComp.vue
+++ src/components/AboutComp.vue
@@ -1,15 +1,35 @@
 <template>
-    <div>
-
-    </div>
+    <section>
+        <div class="container-fluid">
+            <div class="row h-100">
+                <div class="col-6 d-flex align-items-center">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Labore ab laboriosam voluptatibus aperiam eaque rem expedita itaque? Ratione quod doloremque minima corrupti beatae sed eius aliquid tempore mollitia voluptatum. Nemo.</div>
+                <div class="col-6">
+                    <div class="about-img"></div>
+                </div>
+            </div>
+        </div>
+    </section>
 </template>
 
 <script>
     export default {
+        name: 'AboutComp'
         
     }
 </script>
 
 <style lang="scss" scoped>
-@import '../style/style.scss'
+@import '../style/style.scss';
+section{
+    height: 90vh;
+    background-color: #111117;
+    color: white;
+}
+.about-img{
+    background-image: url('../assets/img/about-4.jpg');
+    height:90vh;
+    object-fit: contain;
+    
+}
+
 </style>
