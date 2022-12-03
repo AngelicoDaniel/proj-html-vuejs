1cfa7adba1ce62084075dd1e678cda24ec77b195
Daniel <angelicodaniel93@gmail.com>
2022-12-03 12:05:01 +0100

    the company, statico

--- src/components/AboutComp.vue
+++ src/components/AboutComp.vue
@@ -2,7 +2,30 @@
     <section>
         <div class="container-fluid">
             <div class="row h-100">
-                <div class="col-6 d-flex align-items-center">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Labore ab laboriosam voluptatibus aperiam eaque rem expedita itaque? Ratione quod doloremque minima corrupti beatae sed eius aliquid tempore mollitia voluptatum. Nemo.</div>
+                <div class="col-6 d-flex">
+                <div class="container w-50 m-auto">
+                    <h5 class="py-2">ABOUT THE NETWORK</h5>
+                    <h3><span class="highlighted-white">The</span> Company</h3>
+                    <p>For 12 years we have been providing audit and warranty, financial advice, risk advice, taxes and related services to select clients.</p>
+
+                    <div class="row">
+                        <div class="col-6">
+                            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Atque vel incidunt, iusto consectetur dolore, alias esse facere blanditiis voluptatum minus adipisci nemo voluptate unde, quia exercitationem cumque nostrum. Necessitatibus, animi.
+                        </div>
+                        <div class="col-6">
+                            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Atque vel incidunt, iusto consectetur dolore, alias esse facere blanditiis voluptatum minus adipisci nemo voluptate unde, quia exercitationem cumque nostrum. Necessitatibus, animi.
+                        </div>
+                        <div class="col-6">
+                            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Atque vel incidunt, iusto consectetur dolore, alias esse facere blanditiis voluptatum minus adipisci nemo voluptate unde, quia exercitationem cumque nostrum. Necessitatibus, animi.
+                        </div>
+                        <div class="col-6">
+                            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Atque vel incidunt, iusto consectetur dolore, alias esse facere blanditiis voluptatum minus adipisci nemo voluptate unde, quia exercitationem cumque nostrum. Necessitatibus, animi.
+                        </div>
+                    </div>
+                    <button>GET IN TOUCH</button>
+                    <button>READ MORE</button>
+                </div>
+                </div>
                 <div class="col-6">
                     <div class="about-img"></div>
                 </div>
