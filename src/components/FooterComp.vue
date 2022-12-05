edfa383291f7555fd46352f157ec66a331e05313
Daniel <angelicodaniel93@gmail.com>
2022-12-05 10:42:27 +0100

    inizio footerComp

---
+++ src/components/FooterComp.vue
@@ -0,0 +1,20 @@
+<template>
+    <section class="container w-75">
+        <div class="row">
+            <div class="col-3">lorem</div>
+            <div class="col-3">lorem</div>
+            <div class="col-3">lorem</div>
+            <div class="col-3">lorem</div>
+        </div>
+    </section>
+</template>
+
+<script>
+    export default {
+        name: 'FooterComp'
+    }
+</script>
+
+<style lang="scss" scoped>
+@import '../style/style.scss';
+</style>
