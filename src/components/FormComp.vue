b523e384d21b7e60a542d3af1b987a8a790a3a83
Daniel <angelicodaniel93@gmail.com>
2022-12-05 09:35:21 +0100

    ancora form

--- src/components/FormComp.vue
+++ src/components/FormComp.vue
@@ -1,7 +1,36 @@
 <template>
     <section class="container">
-        <div class="row">
-            <div class="col-8">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloremque, ab? Amet perferendis minima tempora iste provident ipsum sint, est exercitationem quibusdam, ipsam eum officia laboriosam voluptates dolore tempore. Dolor, vero!</div>
+        <div class="row align-items-center">
+            <div class="col-8">
+                <h5>SEND A MESSAGE</h5>
+                <h3>Get in <span class="highlighted-green">Touch</span></h3>
+                <p>We will respond to your message as soon as possibile</p>
+                <div class="container">
+                    <form class="row">
+                        <div class="col-6">
+                            <input type="text" v-model="InputName" placeholder="Name">
+                        </div>
+                        <div class="col-6">
+                            <input type="email" v-model="InputEmail" placeholder="Email">
+                        </div>
+                        <div class="col-6">
+                            <input type="number" v-model="inputPhone" placeholder="Phone">
+                        </div>
+                        <div class="col-6">
+                            <select name="More info" id="">
+                                <option value="opt-1">option 1</option>
+                                <option value="opt-2">option 2</option>
+                                <option value="opt-3">option 3</option>
+                            </select>
+                        </div>
+
+                        <div class="col">
+                            <input type="text-area" class="col-11 m-auto">
+                        </div>
+                    </form>
+
+                </div>
+            </div>
             <div class="col-4">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequuntur consectetur totam illo voluptatum! Et eos ad dolore fugiat unde, quia veritatis molestias doloremque nam, aliquid eveniet aperiam sit quae ut.</div>
         </div>
 
